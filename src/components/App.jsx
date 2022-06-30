b5c33fce3d9d17b5aec6d0e303ed5032e0d44023
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-06-30 17:28:09 +0300

    create TransactionHistory component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
 import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 
 import user from 'path/to/user.json';
 import data from 'path/to/data.json';
 import friends from "path/to/friends.json";
+import transactions from "path/to/transactions.json";
 
 const App = () => {
   return (
@@ -17,8 +19,8 @@ const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      {/* <Statistics stats={data} /> */}
-      <FriendList friends={friends} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
